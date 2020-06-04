5a9cc67878f449c3c15741bb6bed1ec294e2f8e9
lukecool@gmail.com <lukecool@gmail.com>
2020-06-04 00:06:29 -0500

    Adjustments to the Title of the Page/Window (To match new name seen in Browser)

--- disneyplustemplate.py
+++ disneyplustemplate.py
@@ -5,14 +5,14 @@ DTemplate = string.Template("""
 Run, chrome.exe $sUrl
 Sleep, 5000
 SetTitleMatchMode 2
-WinMove, Disney+ - Google Chrome, , 0, 0, 640,480 ;
-Sleep, 5000
-MouseClick, left, 590,397
-Sleep, 500
-MouseClick, left, 590,397
+WinMove, Disney+ | Video Player - Google Chrome, , 0, 0, 640,480 ;
+Sleep, 6000
+Click, 590,397
+Sleep 100
+Click, 590,397
 Sleep, $sLength
 SetTitleMatchMode 2
-IfWinExist Disney+ - Google Chrome
+IfWinExist Disney+ | Video Player - Google Chrome
 		WinClose
 Sleep, 5000
 
