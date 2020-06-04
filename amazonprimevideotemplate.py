5a29ac3794e345feff7efdab32ec5afa439dc003
lukecool@gmail.com <lukecool@gmail.com>
2020-06-04 00:22:07 -0500

    Made title Matching Clearly Match Chrome Browser, Adjustments to Help Ensure autoplay and fullscreen. Still may be issues when amazon inserts an ad before a video

--- amazonprimevideotemplate.py
+++ amazonprimevideotemplate.py
@@ -5,14 +5,15 @@ APVTemplate = string.Template("""
 Run, chrome.exe $sUrl
 Sleep, 5000
 SetTitleMatchMode 2
-WinMove, Prime Video, , 0, 0, 640,480 ;
+WinMove, Prime Video - Google Chrome, , 0, 0, 640,480
+WinActivate, Prime Video - Google Chrome
 Sleep 5000
-Send {space}
+Click, 312,280
 Sleep, 15000
 Send {f}
 Sleep, $sLength
 SetTitleMatchMode 2
-IfWinExist Prime Video
+IfWinExist Prime Video - Google Chrome
 		WinClose
 Sleep, 5000
 
